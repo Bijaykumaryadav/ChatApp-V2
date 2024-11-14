7a1d072f8de3fb88c3bdaa7bee64f7e1fd6255ed
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-11-14 23:35:44 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -69,19 +69,3 @@ const server = app.listen(port,() => {
 })
 
 chatSockets(server);
-
-/*---------------------DEPLOYMENT-----------------------*/
-
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Running on development");
-  });
-}
-
-/*---------------------DEPLOYMENT-----------------------*/
